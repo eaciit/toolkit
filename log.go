f1ecc3342b91cefd1070de2e946c90f569abbe77
juragan360 <ariefda@hotmail.com>
2015-06-04 14:13:00 +0700

    Add Log & Encode Decode

---
+++ log.go
@@ -0,0 +1,103 @@
+package toolkit
+
+import (
+	"fmt"
+	//"io"
+	"errors"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+type LogEngine struct {
+	LogToStdOut     bool
+	LogToFile       bool
+	Path            string
+	FileNamePattern string
+	UseDateFormat   string
+	logInfo         *log.Logger
+	logWarn         *log.Logger
+	logError        *log.Logger
+	//logFile         *log.Logger
+	//logFileHandler  *os.File
+}
+
+func NewLog(toStdOut bool, toFile bool, path string, fileNamePattern string, useDateFormate string) (*LogEngine, error) {
+	var e error
+	l := new(LogEngine)
+	l.LogToStdOut = toStdOut
+	l.LogToFile = toFile
+	l.Path = path
+	l.FileNamePattern = fileNamePattern
+	l.UseDateFormat = useDateFormate
+	//l.logger = log.New(out, prefix, flag)
+
+	e = l.initLogger()
+	if e != nil {
+		return nil, e
+	}
+	return l, nil
+}
+
+func (l *LogEngine) initLogger() error {
+	//var e error = nil
+	if l.LogToStdOut {
+		l.logError = log.New(os.Stdout, "ERROR ", log.Ldate|log.Ltime)
+		l.logInfo = log.New(os.Stdout, "INFO ", log.Ldate|log.Ltime)
+		l.logWarn = log.New(os.Stdout, "WARNING ", log.Ldate|log.Ltime)
+	}
+	return nil
+}
+
+func (l *LogEngine) AddLog(msg string, logtype string) error {
+	var e error
+	logtype = strings.ToUpper(logtype) + " "
+
+	if l.LogToStdOut {
+		if logtype == "ERROR " {
+			l.logError.Println(msg)
+		} else if logtype == "WARNING " {
+			l.logWarn.Println(msg)
+		} else {
+			l.logInfo.Println(msg)
+		}
+		if e != nil {
+			return errors.New("Log.AddLog Error: " + e.Error())
+		}
+	}
+
+	if l.LogToFile {
+		filename := l.FileNamePattern
+		if l.UseDateFormat != "" {
+			filename = fmt.Sprintf(l.FileNamePattern, time.Now().Format(l.UseDateFormat))
+		}
+		filename = filepath.Join(l.Path, filename)
+		f, e := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+		if e != nil {
+			return errors.New("Log.AddLog Error: " + e.Error())
+		}
+		defer f.Close()
+		logFile := log.New(f, logtype, log.Ldate|log.Ltime)
+		logFile.Println(msg)
+	}
+
+	return nil
+}
+
+func (l *LogEngine) Info(msg string) error {
+	return l.AddLog(msg, "INFO")
+}
+
+func (l *LogEngine) Error(msg string) error {
+	return l.AddLog(msg, "ERROR")
+}
+
+func (l *LogEngine) Warning(msg string) error {
+	return l.AddLog(msg, "WARNING")
+}
+
+func (l *LogEngine) Close() {
+	//l.logFileHandler.Close()
+}
--- toolkit.go
+++ toolkit.go
@@ -1,14 +1,73 @@
 package toolkit
 
 import (
+	"bytes"
+	"encoding/gob"
+	"net"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func PathDefault(removeSlash bool) string {
 	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	//dir, _ := os.Getwd()
 	if removeSlash == false {
 		dir = dir + "/"
 	}
 	return dir
+}
+
+func DecodeByte(bytesData []byte, result interface{}) error {
+	buf := bytes.NewBuffer(bytesData)
+	dec := gob.NewDecoder(buf)
+	e := dec.Decode(result)
+	return e
+}
+
+func GetEncodeByte(obj interface{}) []byte {
+	b, e := EncodeByte(obj)
+	if e != nil {
+		return new(bytes.Buffer).Bytes()
+	}
+	return b
+}
+
+func EncodeByte(obj interface{}) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	gw := gob.NewEncoder(buf)
+	err := gw.Encode(obj)
+	if err != nil {
+		return buf.Bytes(), err
+	}
+	return buf.Bytes(), nil
+}
+
+func GetIP() ([]string, error) {
+
+	ret := make([]string, 0)
+	he := func(err error) ([]string, error) {
+		return ret, err
+	}
+
+	ifaces, err := net.Interfaces()
+	he(err)
+
+	// handle err
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		he(err)
+
+		for _, addr := range addrs {
+			interfaceTxt := addr.String()
+			if strings.HasSuffix(interfaceTxt, "24") {
+				interfaceTxt = interfaceTxt[0 : len(interfaceTxt)-3]
+				ret = append(ret, interfaceTxt)
+			}
+		}
+	}
+	if len(ret) == 0 {
+		ret = append(ret, "127.0.0.1")
+	}
+	return ret, nil
 }
