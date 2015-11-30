a553182de37be8e9452c8203f9decd9712679729
juragan360 <ariefda@hotmail.com>
2015-11-30 12:51:14 +0700

    add GenerateRandomString

--- encryption.go
+++ encryption.go
@@ -10,3 +10,17 @@ func MD5String(s string) string {
 	h.Write([]byte(s))
 	return hex.EncodeToString(h.Sum(nil))
 }
+
+func GenerateRandomString(baseChars string, n int) string {
+	if baseChars == "" {
+		baseChars = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnpqrstuvwxyz@#$%&*-_="
+	}
+	baseCharsLen := len(baseChars)
+
+	rnd := ""
+	for i := 0; i < n; i++ {
+		x := RandInt(baseCharsLen)
+		rnd += string(baseChars[x])
+	}
+	return rnd
+}
