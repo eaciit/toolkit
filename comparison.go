4f2b3af99806e4f12171df3ba197b2c0b585934c
Arief Darmawan <ariefda@hotmail.com>
2015-12-07 05:48:06 +0700

    add invalid

--- comparison.go
+++ comparison.go
@@ -1,5 +1,21 @@
 package toolkit
 
+import (
+	"reflect"
+)
+
+func IsValid(o interface{}) bool {
+	//t := reflect.TypeOf(o)
+	v := reflect.ValueOf(o)
+	if !v.IsValid() {
+		return false
+	}
+	if v.IsNil() {
+		return false
+	}
+	return true
+}
+
 func IfEq(has interface{}, want interface{}, a interface{}, b interface{}) interface{} {
 	if has == want {
 		return a
