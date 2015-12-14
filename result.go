095a1994f18f317e262df6f7e46ac104a6f31052
juragan360 <ariefda@hotmail.com>
2015-12-14 15:18:57 +0700

    Add CallResult

--- result.go
+++ result.go
@@ -2,6 +2,7 @@ package toolkit
 
 import (
 	"errors"
+	"fmt"
 	"time"
 )
 
@@ -84,4 +85,21 @@ func (a *Result) Run(f func(data interface{}) (interface{}, error), parm interfa
 	}
 	a.Duration = time.Since(t0)
 	return a
+}
+
+func CallResult(url, calltype string, data []byte) (*Result, error) {
+	r, e := HttpCall(url, calltype, data, M{}.Set("expectedstatus", 200))
+	if e != nil {
+		return nil, fmt.Errorf(url + " Call eror: " + e.Error())
+	}
+
+	result := NewResult()
+	edecode := Unjson(HttpContent(r), &result)
+	if edecode != nil {
+		return nil, fmt.Errorf(url + " Decode error: " + edecode.Error())
+	}
+	if result.Status == Status_NOK {
+		return result, fmt.Errorf(result.Message)
+	}
+	return result, nil
 }
