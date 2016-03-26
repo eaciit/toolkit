cfa7b11da62eb3fc82e796c60db67e05ce28c032
Arief Darmawan <ariefda@hotmail.com>
2016-03-26 18:37:36 +0700

    add error

---
+++ error.go
@@ -0,0 +1,13 @@
+package toolkit
+
+import (
+	"fmt"
+)
+
+func Error(txt string) error {
+	return fmt.Errorf(txt)
+}
+
+func Errorf(txt string, obj ...interface{}) error {
+	return fmt.Errorf(txt, obj)
+}
