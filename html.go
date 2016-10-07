612bd1d93bada71276eacd4f3a5f6a648140dc1a
novalagung <caknopal@gmail.com>
2016-10-07 11:00:27 +0700

    - improve parser

--- html.go
+++ html.go
@@ -7,19 +7,20 @@ import (
 )
 
 func Html2text(source string) (string, error) {
+	// ========= Parse the HTML
+
 	doc, err := html.Parse(strings.NewReader(source))
 	if err != nil {
 		return "", err
 	}
 
 	res := ""
-
 	var f func(n *html.Node)
 	f = func(n *html.Node) {
 		switch n.Type {
 		case html.ElementNode:
 			switch n.Data {
-			case "li", "br":
+			case "li", "br", "p", "div", "hr":
 				res = fmt.Sprintf("%s\n", res)
 			}
 		case html.TextNode:
@@ -31,5 +32,18 @@ func Html2text(source string) (string, error) {
 	}
 	f(doc)
 
-	return strings.TrimSpace(res), nil
+	// ========= Replace double spaces
+
+	resFinal := ""
+	for _, each := range strings.Split(res, "\n") {
+		each := strings.TrimSpace(each)
+
+		if each == "" {
+			continue
+		}
+
+		resFinal = fmt.Sprintf("%s\n%s", resFinal, each)
+	}
+
+	return strings.TrimSpace(resFinal), nil
 }
